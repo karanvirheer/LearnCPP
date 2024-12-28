1827983769ba67a7ee8a9d3043b5e4f19a15d964
karanvirheer <karanvirheer77@gmail.com>
2024-12-27 22:18:05 -0500

    Added cpp DAP support

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <ostream>
 
-int main()
-{
-    std::cout << "Hello World";
-    return 0;
+int main() {
+  int x = 0;
+
+  if (x == 5) {
+    std::cout << "x is 5" << std::endl;
+  } else {
+    std::cout << "x is not 5" << std::endl;
+  }
+
+  for (int i = 0; i < 10; i++) {
+    x += 1;
+  }
+
+  std::cout << "Hello World" << std::endl;
+  return 0;
 }
